598e9ac82aca85eb14a8d3603b74160af7f4d7cd
Ayush Gupta <ayushgupta1925@gmail.com>
2025-10-02 20:05:10 +0530

    Update reviewRoute.js

--- backend/route/reviewRoute.js
+++ backend/route/reviewRoute.js
@@ -1,11 +1,14 @@
 import express from "express";
 import isAuth from "../middleware/isAuth.js";
-import { createReview, getReview } from "../controller/reviewController.js";
+import { createReview,getCourseReviews,getReview } from "../controller/reviewController.js";
 
 const reviewRouter = express.Router();
 
 reviewRouter.post("/createreview", isAuth, createReview);
 
 reviewRouter.get("/getreview", isAuth, getReview);
+
+// ✅ new route: get reviews only for one course
+reviewRouter.get("/course/:courseId", getCourseReviews);
 
 export default reviewRouter;
